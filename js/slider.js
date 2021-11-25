12e8ac3d4e4b14afdf2cd9978823756cad0c3c95
Bui Duc Sone <buiducson171001@gmail.com>
2021-11-25 11:25:31 +0700

    update

--- js/slider.js
+++ js/slider.js
@@ -7,3 +7,21 @@ $('.slick-banner').slick({
     arrows: false,
     
   });
+  $('.feedback-slick').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-right"></i></button>',
+    nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-left"></i></button>'
+  });
+  $('.partner-slick').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-right"></i></button>',
+    nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-left"></i></button>'
+  });
