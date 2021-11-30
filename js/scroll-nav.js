969c609170908fca6696a37995fd2a65146c906d
Bui Duc Sone <buiducson171001@gmail.com>
2021-11-30 17:11:19 +0700

    comment

--- js/scroll-nav.js
+++ js/scroll-nav.js
@@ -1,3 +1,4 @@
+AOS.init();
 $(document).ready(function(){
     $(window).scroll(function(){
         if ($(this).scrollTop()){
@@ -7,3 +8,22 @@ $(document).ready(function(){
         )
     })
 })
+
+// nav
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
--- js/slider.js
+++ js/slider.js
@@ -10,12 +10,12 @@ $('.slick-banner').slick({
   $('.feedback-slick').slick({
     dots: false,
     infinite: true,
-    speed: 300,
+    speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-right"></i></button>',
-    nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-left"></i></button>'
+    nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-left"></i></button>',
   });
   $('.partner-slick').slick({
     slidesToShow: 6,
@@ -23,5 +23,33 @@ $('.slick-banner').slick({
     autoplay: false,
     autoplaySpeed: 2000,
     prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-right"></i></button>',
-    nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-left"></i></button>'
+    nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-left"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint:576,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]  
   });
